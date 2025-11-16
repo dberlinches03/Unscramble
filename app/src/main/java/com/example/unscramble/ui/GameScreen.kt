5e49fbe8dc61e25119b677a693cbae635be93cd1
dberl <dberlinches2003@gmail.com>
2025-11-16 23:36:53 +0100

    Pagina 9 codelab, pasos del 6 al 8

--- app/src/main/java/com/example/unscramble/ui/GameScreen.kt
+++ app/src/main/java/com/example/unscramble/ui/GameScreen.kt
@@ -120,6 +120,13 @@ fun GameScreen(
             }
         }
 
+        if (gameUiState.isGameOver) {
+            FinalScoreDialog(
+                score = gameUiState.score,
+                onPlayAgain = { gameViewModel.resetGame() }
+            )
+        }
+
         GameStatus(score = gameUiState.score, modifier = Modifier.padding(20.dp))
     }
 }
