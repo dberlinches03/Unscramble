28cdaedfeec44ac08e2d2804ff9e3b2c84a326e4
dberl <dberlinches2003@gmail.com>
2025-11-16 22:35:50 +0100

    Pagina 7 codelab, paso 10 agregada variable score en GameUiState.kt

--- app/src/main/java/com/example/unscramble/ui/GameUiState.kt
+++ app/src/main/java/com/example/unscramble/ui/GameUiState.kt
@@ -5,5 +5,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 data class GameUiState(
     val currentScrambledWord: String = "",
     val isGuessedWordWrong: Boolean = false,
+    val score: Int = 0
 )
 private val _uiState = MutableStateFlow(GameUiState())
