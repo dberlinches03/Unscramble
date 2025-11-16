73fcf2d825e1b34934c82e75c884b6f59ba6bb39
dberl <dberlinches2003@gmail.com>
2025-11-16 22:04:48 +0100

    Página 7, Paso 3

--- app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
+++ app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -8,6 +8,7 @@ import com.example.unscramble.data.allWords
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class GameViewModel : ViewModel () {
     private val  _uiState = MutableStateFlow(GameUiState())
@@ -49,9 +50,13 @@ class GameViewModel : ViewModel () {
     fun  checkUserGuess() {
         if (userGuess.equals(currentWord, ignoreCase = true)) {
         } else {
+            _uiState.update { currentState ->
+                currentState.copy(isGuessedWordWrong = true)
+            }
         }
         updateUserGuess("")
     }
+
 
     fun resetGame() {
         usedWords.clear()
