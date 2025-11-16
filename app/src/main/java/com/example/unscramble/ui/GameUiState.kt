4d4ad6ba48f6e371b2d2b0ccddcb1dc0419672d9
dberl <dberlinches2003@gmail.com>
2025-11-16 23:30:29 +0100

    Pagina 9 codelab, pasos del 1 al 5

--- app/src/main/java/com/example/unscramble/ui/GameUiState.kt
+++ app/src/main/java/com/example/unscramble/ui/GameUiState.kt
@@ -6,6 +6,7 @@ data class GameUiState(
     val currentScrambledWord: String = "",
     val currentWordCount: Int = 1,
     val isGuessedWordWrong: Boolean = false,
+    val isGameOver: Boolean = false,
     val score: Int = 0
 )
 private val _uiState = MutableStateFlow(GameUiState())
